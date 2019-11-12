71a97c59294784a78385aa6b843cbc2a43dfcaa1
南部遼 <>
2019-11-12 23:57:19 +0800

    宿題追加
    自作のタブメニューと関連画像を追加

--- assets/js/app.js
+++ assets/js/app.js
@@ -141,25 +141,32 @@ $(function () {
 
 
     //selfタブメニュー
-    $('.tab-nav2 a').on('click', function () {
+    $('.menu-tab a').on('click', function () {
         if ($(this).hasClass('active')) {
             return false;
         }
 
         console.log(this.hash);
 
-        // タブメニューを初期化
-        $('.tab-nav2 a').removeClass('active');
+        // var type = window.location.hash.substr(1);
+        // console.log(type);
+
+        // var urlHash = location.hash;
+        // console.log(urlHash);
+
+        // $('.image[src="assets/img/menu01.jpg"]').attr('src',"assets/img/" + type + ".jpg");
+
+        $('.menu-tab a').removeClass('active');
         $(this).addClass('active');
 
-        // タブコンテンツを初期化
-        $('.tab-content2 > div').removeClass('active');
-        $('.tab-content2 > div')
-            //tab-content直下のdivの中から、同じhashのclassを探し、activeにする
+        $('.menu-content > div').removeClass('active');
+        $('.menu-content > div')
             .filter(this.hash)
             .addClass('active');
 
         return false;
+
+ 
     });
 
 
